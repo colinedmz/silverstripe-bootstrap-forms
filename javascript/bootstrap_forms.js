93d471239ff27f9800101abd6c94752dcd7fd790
Coline Demazure <coline@catalyst.net.nz>
2015-08-06 11:45:22 +1200

    Add Wakes work on Unclecheese work

--- javascript/bootstrap_forms.js
+++ javascript/bootstrap_forms.js
@@ -1,57 +1,80 @@
 (function($) {
-$(function() {
-	if($('textarea.wysiwyg').length) {
-		$('textarea.wysiwyg').each(function() {
-			var $t = $(this);
-		    $t.tinymce({
-		      theme: "advanced",
-		      theme_advanced_toolbar_location: "top",
-		      theme_advanced_buttons1: $t.attr('data-buttons'),
-		      theme_advanced_buttons2: "",
-		      theme_advanced_buttons3: "",
-		      theme_advanced_blockformats: $t.attr('data-blockformats'),
-		      content_css: ($t.attr('data-css') ? ($('base')).attr('href') + $t.attr('data-css') : null)
-		    });
+	$(function() {
+		// dropdowns need to be configured when javascript is enabled.
+		$('.dropdown-select').hide();
+		$('.dropdown-toggle').show();
+
+		// sync selected list item with the select control
+		$('.dropdown-menu a').on('click', function(ev) {
+			var rel = $(this).attr('rel'),
+				value = $(this).data('value');
+
+			$(rel + '_select').val(value);
+			$(rel + '_select').change();
+			$(rel + '_label').text($(this).text());
 		});
-	}
+		// for optionsets hide the actual radio input if javascript enabled
+		$('.optionset input[type="radio"]').hide();
 
-	
-	
-	if($('select.chosen').length) {
-	    $('select.chosen').chosen({
-	      disable_search_threshold: $(this).attr('data-search-threshold')
-	    });
-    }
-
-
-	if($('textarea[maxlength]').length) {		
-		$('textarea[maxlength]').parent().append("<p class='notes charsRemaining'>&nbsp;</p>");
-		$('textarea[maxlength]').keyup(function(){		
-			var charText = $(this).attr('characters-remaining-text');
-			if(!charText) charText = " characters remaining";
-			var max = parseInt($(this).attr('maxlength'));
-			if($(this).val().length > max){
-				$(this).val($(this).val().substr(0, $(this).attr('maxlength')));
-			}
-			$(this).parent().find('.charsRemaining').html((max - $(this).val().length) + " " + charText);
+		// radio buttons need active set on them when the label is clicked
+		$('.optionset label').on('click', function(ev) {
+			$('.btn', $(this).parents('.optionset')).removeClass('active');
+			$(this).parents('.btn').addClass('active');
+			$('input[type="radio"]', $(this)).prop('checked', true);
 		});
-	}
 
 
-	if($('.field.bootstrapbuttongroup').length) {
+		if ($('textarea.wysiwyg').length) {
+			$('textarea.wysiwyg').each(function() {
+				var $t = $(this);
+				$t.tinymce({
+					theme: "advanced",
+					theme_advanced_toolbar_location: "top",
+					theme_advanced_buttons1: $t.attr('data-buttons'),
+					theme_advanced_buttons2: "",
+					theme_advanced_buttons3: "",
+					theme_advanced_blockformats: $t.attr('data-blockformats'),
+					content_css: ($t.attr('data-css') ? ($('base')).attr('href') + $t.attr('data-css') : null)
+				});
+			});
+		}
 
-		$('.field.bootstrapbuttongroup .btn-group button').click(function(e) {
-			e.preventDefault();
-			var $holder = $(this).closest('.field.bootstrapbuttongroup');
-			$holder.find('.active').removeClass('active');
-			$(this).addClass('active');
-			$holder.find(':hidden').val($(this).data('value')).trigger('change');
 
-		}).filter('.active').click();	
-	}
+		if ($('select.chosen').length) {
+			$('select.chosen').chosen({
+				disable_search_threshold: $(this).attr('data-search-threshold')
+			});
+		}
 
 
-  });
+		if ($('textarea[maxlength]').length) {
+			$('textarea[maxlength]').parent().append("<p class='notes charsRemaining'>&nbsp;</p>");
+			$('textarea[maxlength]').keyup(function() {
+				var charText = $(this).attr('characters-remaining-text');
+				if (!charText) charText = " characters remaining";
+				var max = parseInt($(this).attr('maxlength'));
+				if ($(this).val().length > max) {
+					$(this).val($(this).val().substr(0, $(this).attr('maxlength')));
+				}
+				$(this).parent().find('.charsRemaining').html((max - $(this).val().length) + " " + charText);
+			});
+		}
+
+
+		if ($('.field.bootstrapbuttongroup').length) {
+
+			$('.field.bootstrapbuttongroup .btn-group button').click(function(e) {
+				e.preventDefault();
+				var $holder = $(this).closest('.field.bootstrapbuttongroup');
+				$holder.find('.active').removeClass('active');
+				$(this).addClass('active');
+				$holder.find(':hidden').val($(this).data('value')).trigger('change');
+
+			}).filter('.active').click();
+		}
+
+
+	});
 
 
 })(jQuery);
